80926db922a182c107b9f93bb0a0270994898619
AkshayaBanswada <92936598+AkshayaBanswada@users.noreply.github.com>
2021-10-21 22:35:32 +0530

    created area.py
    we got area after code is executed

---
+++ Area.py
@@ -0,0 +1,5 @@
+#program to input the radius of a circle
+r = input("Input the radius of the circle : ")
+r = float(r)
+a = 3.14 * r**2
+print("The area of the circle with radius "+str(r)+" is : "+str(a))
